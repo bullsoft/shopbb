2327bf8de42f38ed102c47f1aa39a2654e4365f4
Gu Weigang <guweigang@outlook.com>
2019-04-01 14:03:53 +0800

    add initial build script

---
+++ build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+echo "Build start..."
+
+BUILD_DIR=$HOME/tmp/buildhome
+echo "Buiding Dir... ->"$BUILD_DIR;
+PRJ_DIR=$BUILD_DIR/firstPipeline
+DEPLOY_VER=`date +%Y%m%d%H%M%S`
+echo "ReleaseId: "$DEPLOY_VER
+
+APP_DIR=$PRJ_DIR/$DEPLOY_VER
+mkdir -p $APP_DIR
+cp -R . $APP_DIR/
+cd $APP_DIR
+
+echo "Entering App Deploying Dir.. ->"$APP_DIR
+
+echo "-------------------------"
+echo "Detecting PHP version..."
+echo "-------------------------"
+php -v
+
+if [ -f ${APP_DIR}/composer.json ]; then
+    echo "-------------------------"
+    echo "Running composer..."
+    echo "-------------------------"
+    # Run Composer.
+
+    COMPOSER_FLAGS='--no-scripts --no-dev --prefer-dist'
+
+    cd ${APP_DIR} && \
+            $HOME/bin/composer \
+            install \
+            --optimize-autoloader \
+            --no-interaction \
+            --no-ansi \
+            --no-progress \
+            ${COMPOSER_FLAGS}
+
+fi
+
+echo "Build finished."
