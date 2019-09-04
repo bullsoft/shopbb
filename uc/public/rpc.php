37f662594e5a8ccfb4192e5fe93157cf19924719
Weigang <guweigang@bullsoft.org>
2019-09-04 13:54:40 +0800

    add rpc entry

--- uc/public/rpc.php
+++ uc/public/rpc.php
@@ -1,7 +1,12 @@
 <?php
-require_once(dirname(__DIR__)."/app/plugins/HarServer.php");
+// require_once(dirname(__DIR__)."/app/plugins/HarServer.php");
+// $app = new LightCloud\Uc\Plugins\HarServer();
+$di = new \Phalcon\DI\FactoryDefault();
 $bootstrap = new \PhalconPlus\Bootstrap(dirname(__DIR__));
-$app = new LightCloud\Uc\Plugins\HarServer();
-echo $bootstrap->setApp($app, true)
-               ->execSrv()
-               ->getContent();
+$app = new \PhalconPlus\Rpc\Yar($di);
+
+$bootstrap->setDi($di);
+$response = $bootstrap->initConf()
+                      ->setApp($app, true)
+                      ->execSrv();
+echo $response->getContent();
